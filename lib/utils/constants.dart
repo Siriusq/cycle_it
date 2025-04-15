c4843e807226286d6455c170a17a0148a6dbd991
Siriusq <eargon.skyle@gmail.com>
2025-04-16 00:06:00 +0800

    Add Neumorphism Design

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 // All of our constant stuff
 
-const kPrimaryColor = Color(0xFF366CF6);
+const kPrimaryColor = Color(0xFF57E2CC);
 const kSecondaryColor = Color(0xFFF5F6FC);
 const kBgLightColor = Color(0xFFF2F4FC);
 const kBgDarkColor = Color(0xFFEBEDFA);
