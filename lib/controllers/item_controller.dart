b090cd39731cdefb8f1a1daec08ab04829e4aea8
Siriusq <eargon.skyle@gmail.com>
2025-05-28 01:20:53 +0800

    Add edit and delete usage record function to item controller

--- lib/controllers/item_controller.dart
+++ lib/controllers/item_controller.dart
@@ -57,4 +57,113 @@ class ItemController extends GetxController {
 
     // todo: 持久化数据 saveItemToDatabase(currentItem.value);
   }
+
+  // 编辑使用记录
+  void editUsageRecord(int recordId, DateTime newUsedAt) {
+    final currentItemValue = currentItem.value!;
+    final List<UsageRecordModel> records = List.from(
+      currentItemValue.usageRecords,
+    );
+    final int index = records.indexWhere(
+      (record) => record.id == recordId,
+    );
+
+    if (index != -1) {
+      // 1. 更新当前被编辑的记录
+      records[index] = UsageRecordModel(
+        id: records[index].id,
+        itemId: records[index].itemId,
+        usedAt: newUsedAt,
+        intervalSinceLastUse: null, // 暂时清空，重新计算
+      );
+
+      // 重新排序（因为日期可能改变了）
+      records.sort((a, b) => a.usedAt.compareTo(b.usedAt));
+
+      // 2. 重新计算受影响的记录的时间间隔
+      List<UsageRecordModel> updatedRecords = [];
+      for (int i = 0; i < records.length; i++) {
+        int? interval;
+        if (i > 0) {
+          interval =
+              records[i].usedAt
+                  .difference(records[i - 1].usedAt)
+                  .inDays;
+        }
+        updatedRecords.add(
+          UsageRecordModel(
+            id: records[i].id,
+            itemId: records[i].itemId,
+            usedAt: records[i].usedAt,
+            intervalSinceLastUse: interval,
+          ),
+        );
+      }
+
+      // 更新 ItemModel
+      currentItem.value = ItemModel(
+        id: currentItemValue.id,
+        name: currentItemValue.name,
+        usageComment: currentItemValue.usageComment,
+        usageRecords: records,
+        // 使用更新后的列表
+        tags: currentItemValue.tags,
+        iconPath: currentItemValue.iconPath,
+        iconColor: currentItemValue.iconColor,
+        notifyBeforeNextUse: currentItemValue.notifyBeforeNextUse,
+      )..invalidateCalculatedProperties(); // 清除缓存
+
+      // todo: 持久化数据 saveItemToDatabase(currentItem.value);
+    }
+  }
+
+  // 删除使用记录
+  void deleteUsageRecord(int recordId) {
+    final currentItemValue = currentItem.value!;
+    final List<UsageRecordModel> records = List.from(
+      currentItemValue.usageRecords,
+    );
+    final int index = records.indexWhere(
+      (record) => record.id == recordId,
+    );
+
+    if (index != -1) {
+      records.removeAt(index);
+
+      // 重新计算所有记录的时间间隔
+      List<UsageRecordModel> updatedRecords = [];
+      for (int i = 0; i < records.length; i++) {
+        int? interval;
+        if (i > 0) {
+          interval =
+              records[i].usedAt
+                  .difference(records[i - 1].usedAt)
+                  .inDays;
+        }
+        updatedRecords.add(
+          UsageRecordModel(
+            id: records[i].id,
+            itemId: records[i].itemId,
+            usedAt: records[i].usedAt,
+            intervalSinceLastUse: interval,
+          ),
+        );
+      }
+
+      // 更新 ItemModel
+      currentItem.value = ItemModel(
+        id: currentItemValue.id,
+        name: currentItemValue.name,
+        usageComment: currentItemValue.usageComment,
+        usageRecords: records,
+        // 使用更新后的列表
+        tags: currentItemValue.tags,
+        iconPath: currentItemValue.iconPath,
+        iconColor: currentItemValue.iconColor,
+        notifyBeforeNextUse: currentItemValue.notifyBeforeNextUse,
+      )..invalidateCalculatedProperties(); // 清除缓存
+
+      // todo: 持久化数据 saveItemToDatabase(currentItem.value);
+    }
+  }
 }
